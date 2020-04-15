2cd97b3a6a210bbcc2d8aded784a3ce709f4bcb4
javitocor <javitocor@gmail.com>
2020-04-15 19:09:18 +0200

    Added average rating

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,8 +1,17 @@
 module ReviewsHelper
-    def total_rating
-        @reviews = Review.all
-        
-        total = @reviews[:rating].average
-        return total
+    def total_rating(id)
+        reviews = Review.all 
+        total = []
+        reviews.each do |x|
+            if x[:movie_id] == id
+                total.push(x[:rating])
+            end
+        end
+        if total != nil && total != []
+            aver = (total.reduce(:+) / total.size).round(1)
+        else
+            p 'No rating yet'
+        end   
     end
 end
+
