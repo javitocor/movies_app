7f41b6d38d9cbc7fef5c1540803702baf613d07d
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-04-16 11:55:20 +0530

    optimized rating average helper

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,13 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.create(review_params)
     @review.movie_id = params[:movie_id]
-     if @review.save
+    if @review.save
       flash.notice = 'Your review has been created'
       redirect_to movie_path(@review.movie_id)
-     else
-       flash.notice ='ERROR: Review was not created, you must fill up all the fields'
-       redirect_to movie_path(@review.movie_id)
-     end
+    else
+      flash.notice ='ERROR: Review was not created, you must fill up all the fields'
+      redirect_to movie_path(@review.movie_id)
+    end
   end
 
   private
